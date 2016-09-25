c758c8ba898c793104d99e0fa7b8fee6f3bb4306
Pawel Galazka <pawel.galazka1986@gmail.com>
2016-09-25 14:42:41 +1000

    runfile: making tests more isolated

--- runfile.js
+++ runfile.js
@@ -4,6 +4,19 @@ import path from 'path'
 const FIRENPM_PATH = path.resolve('./packages/firenpm')
 const FIRENPM_SCRIPT = path.resolve('./packages/firenpm.cli/bin/firenpm.js')
 
+function isolated(callback) {
+  run('mv ./node_modules ./.node_modules') // sandbox should not read modules from directory above
+  run('mv ./package.json ./.package.json')
+  try {
+    callback()
+  } catch (e) {
+    throw e.stack
+  } finally {
+    run('mv ./.package.json ./package.json')
+    run('mv ./.node_modules ./node_modules')
+  }
+}
+
 const task = {
   'sandbox:clean': () => {
     run('rm -rf .sandbox')
@@ -14,21 +27,17 @@ const task = {
     run(`(cd .sandbox && FIRENPM_PATH=${FIRENPM_PATH} ${FIRENPM_SCRIPT} test-project)`)
   },
   'sandbox:run': (...cmd) => {
-    run(`(cd .sandbox/test-project && run ${cmd.join(' ')})`)
+    isolated(() => {
+      run(`(cd .sandbox/test-project && run ${cmd.join(' ')})`)
+    })
   },
   'test': () => {
     task['sandbox:clean']()
-    run('mv ./node_modules ./.node_modules') // sandbox should not read modules from directory above
-    try {
+    isolated(() => {
       run(`(cd .sandbox && FIRENPM_PATH=${FIRENPM_PATH} ${FIRENPM_SCRIPT} test-project)`)
       run('(cd .sandbox/test-project && run test)')
       run('(cd .sandbox/test-project && run build)')
-    } catch (e) {
-      throw e.stack
-    } finally {
-      run('mv ./.node_modules ./node_modules')
-      run('rm -rf .sandbox')
-    }
+    })
   }
 }
 
